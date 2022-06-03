f24587981902b0c2787a3e75ad8e154c66af3b42
Adrian <u201718169@upc.edu.pe>
2022-06-03 18:48:15 -0500

    Initial commit

---
+++ src/test/java/com/goback/restore/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goback.restore.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
